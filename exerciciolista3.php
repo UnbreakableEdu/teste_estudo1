c6591965d9391403ad0c8984da6060b630a048b8
Eduardo Onofre <48128069+UnbreakableEdu@users.noreply.github.com>
2019-03-07 09:04:21 -0300

    exercicios

---
+++ exerciciolista3.php
@@ -0,0 +1,14 @@
+<?php
+
+print "O valor de seu salário atual: ";
+$salario = (float) fgets (STDIN);
+
+print "Digite a porcentagem do aumento: ";
+$porc = (float) fgets (STDIN);
+
+$nova_porc = $porc/100;
+
+$valor_aumento = $salario*$nova_porc;
+$novo_salario = $salario+$valor_aumento;
+
+print "O salário atual reajustado é: $novo_salario";
