be29e55d39de1b9f7779fdb1ef38579f39fa8dbf
Eduardo Onofre <48128069+UnbreakableEdu@users.noreply.github.com>
2019-03-08 10:53:26 -0300

    exercicios

--- cigarette.php
+++ cigarette.php
@@ -4,14 +4,12 @@
     $num_de_anos = (int) fgets(STDIN);
 
     print "\n Quantos cigarros por dia?  ";
-    $cigarros = (int) fgets(STDIN);
+    $cigarros_por_dia = (int) fgets(STDIN);
     
     $ano             = 365;
-    $minutos_por_dia = 11*$cigarros;
+    $minutos_por_dia = 10*$cigarros_por_dia;
     $min_totais      = $minutos_por_dia*$ano*$num_de_anos;
     $horas           = $min_totais/60;
     $dias            = round($horas/24);
-    $meses           = round($dias/30);
 
-        print"\n Você perdeu $dias dias de vida, a não ser que você já tenha morrido de câncer  
-        \n Você perdeu aproximadamente $meses meses \n";
+    print"\n Você perdeu $dias dias de vida";
