5d245667cb8a116d7790cbb7ca104bd5dc11205f
Eduardo Onofre <48128069+UnbreakableEdu@users.noreply.github.com>
2019-03-22 11:32:35 -0300

    whileFor

---
+++ tabuada1.php
@@ -0,0 +1,8 @@
+<?php
+    
+    for($tabu=2;$tabu<=10;$tabu++){
+        for($i=1;$i<=10;$i++){
+            $calculo = $tabu * $i;
+            print "{$tabu} x {$i} = {$calculo} \n";
+        }
+    }
